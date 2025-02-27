b79e8a561466b55964303041d01c7091a6824a39
vaishnavi gadhe <gadhevaishnavi70@gmail.com>
2025-02-27 13:55:01 +0530

    added button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../assets/css/Navbar.css";
-import logo from "../assets/images/logo.png";
+import logo from "../assets/images/logo1.webp";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
