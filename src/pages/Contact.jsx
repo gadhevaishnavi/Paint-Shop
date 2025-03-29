17fba61ed1cd364a2edf3940190046ea008d5193
vaishnavi gadhe <gadhevaishnavi70@gmail.com>
2025-03-29 22:30:09 +0530

    change name

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
   return (
     <div className="main-class">
       <h1 className="heading">Contact Us</h1>
-      <p className="name-section">Name: Ramparsad Balaji Shikhare</p>
+      <p className="name-section">Name: Ram Marotrao Shikhare</p>
       <p className="phone-section">Phone No: 8378844361</p>
       <p className="address-section">Address: At Girgaon, Tq: Vasamat, Dist: Hingoli</p>
       <p className="shop-section">Shop Address:Raj Galaxy,shop no 7,SBI Bank opposite Malegaon Road Nanded 413512</p>
